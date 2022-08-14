7304c49cf4e6519f7d5df6afcefceed723a42c24
Leonid <leeonidix@gmail.com>
2022-08-14 18:37:17 +0500

    Task001 Done

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+Console.Write("Enter numbers separated by spaces: ");
+string[] input = Console.ReadLine().Split();
+int[] arrNumbers = new int[input.Length];
+int count = 0;
+for (int i = 0; i < input.Length; i++)
+{
+    arrNumbers[i] = int.Parse(input[i]);
+}
+for (int i = 0; i < arrNumbers.Length; i++)
+{
+    if (arrNumbers[i] > 0)
+    {
+        count++;
+    }
+}
+
+PrintArray(arrNumbers);
+Console.Write($"-> {count}");
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++) 
+    {
+        Console.Write(arr[i] + ", ");
+    }
+}
